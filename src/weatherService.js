aa32de4a5febeb14d4336cdc31c3ace50081b151
Ezequiel Santalla <ezequielasantalla@gmail.com>
2023-03-02 01:13:43 -0600

    weather app first changes

---
+++ src/weatherService.js
@@ -0,0 +1,31 @@
+const API_KEY = "9b00ed87ef63048afd0708aba50aeeb4";
+
+const  makeIconURL = (iconId) => `https://openweathermap.org/img/wn/${iconId}@2x.png`;
+
+const getWeatherData = async (city, units = "metric") => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+  const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+
+  const {weather, main: {feels_like, temp, temp_min, temp_max, pressure, humidity}, wind: {speed}, sys: {country}, name} = data;
+
+  const {description, icon} = weather[0];
+
+  return {
+    description, 
+    iconURL: makeIconURL(icon), 
+    feels_like, 
+    temp, 
+    temp_min, 
+    temp_max, 
+    pressure, humidity, 
+    speed, 
+    country, 
+    name,
+  };
+};
+
+
+export {getWeatherData};
